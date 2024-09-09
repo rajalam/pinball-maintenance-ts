ed6e25cb54c2fbd62329ceb88a528e97b5458a49
rajalam <mikko.rajala@iki.fi>
2024-09-09 18:05:28 +0300

    Main view background CSS dynamic layout defined

--- src/PinballMain.tsx
+++ src/PinballMain.tsx
@@ -4,7 +4,19 @@ function PinballMain() {
 
     return(
         <div className='background'>
+            <div className='app-workspace-background'>
+
+            
             Test
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+             
+
+            </div>            
         </div>
     );
 }
