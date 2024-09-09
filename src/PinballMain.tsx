0c59e17efb87840fcd3235510560172969842533
rajalam <mikko.rajala@iki.fi>
2024-09-09 16:12:49 +0300

    Change init file to PinballMain, some test input to PinballMain.tsx and .css

---
+++ src/PinballMain.tsx
@@ -0,0 +1,12 @@
+import './PinballMain.css';
+
+function PinballMain() {
+
+    return(
+        <div className='background'>
+            Test
+        </div>
+    );
+}
+
+export default PinballMain;
