4f1eec6262fb853d858fb90b132f5b69cdb13486
rajalam <mikko.rajala@iki.fi>
2024-09-13 20:43:49 +0300

    File formatting/refactoring

--- src/components/ShowDeviceMenu.tsx
+++ src/components/ShowDeviceMenu.tsx
@@ -1,21 +1,21 @@
 import './ShowDeviceMenu.css';
 import ShowDeviceMenuItem from './ShowDeviceMenuItem';
 
-function ShowDeviceMenu () {
-    return(
+function ShowDeviceMenu() {
+    return (
 
         <div className="device-list-container">
 
-            <ShowDeviceMenuItem deviceName='Star Wars(Pena)' 
-                onClick={ () => console.log('Button clicked!')}>
-                <div>jlfasjlfsjfslkda</div> 
+            <ShowDeviceMenuItem deviceName='Star Wars(Pena)'
+                onClick={() => console.log('Button clicked!')}>
+                <div>jlfasjlfsjfslkda</div>
                 <div>jlfasjlfsjfslkda</div>
                 <div>jlfasjlfsjfslkda</div>
             </ShowDeviceMenuItem>
 
-            <ShowDeviceMenuItem deviceName='Star Wars(Mika)' 
-                onClick={ () => console.log('Button clicked!')}>
-                <div>jlfasjlfsjfslkda</div> 
+            <ShowDeviceMenuItem deviceName='Star Wars(Mika)'
+                onClick={() => console.log('Button clicked!')}>
+                <div>jlfasjlfsjfslkda</div>
                 <div>jlfasjlfsjfslkda</div>
                 <div>jlfasjlfsjfslkda</div>
             </ShowDeviceMenuItem>
