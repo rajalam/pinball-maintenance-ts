8a9e2e832b563aca0aa7338dd9a1084ef0a81978
rajalam <mikko.rajala@iki.fi>
2024-09-16 16:54:34 +0300

    New type definions, begin reducer cases

--- src/PinballMain.tsx
+++ src/PinballMain.tsx
@@ -1,22 +1,63 @@
 import './PinballMain.css';
 import ShowDeviceMenu from './components/ShowDeviceMenu';
+import { useReducer, useEffect } from 'react';
+import axios from 'axios'; // npm install axios , jos ei ole jo ladattu
 
 type Device = {
-    deviceId: number,
-    name:string    
+    deviceId: number;
+    name:string;
 };
 
 type AppState = {
 
-    isError:boolean,
-    errorMessage: string,
-    fetchDeviceListData:boolean,
-    deviceListFetchCommenced:boolean,
-    deviceListData:[Device]
+    isError:boolean;
+    errorMessage:string;
+    fetchDeviceListData:boolean;
+    deviceListFetchCommenced:boolean;
+    deviceListData:Device[];
 
 };
 
+const initAppState:AppState = {
+    isError: false,
+    errorMessage:'',
+    fetchDeviceListData: true,
+    deviceListFetchCommenced: false,
+    deviceListData: []
+};
+
+
 function PinballMain() {
+    
+    type StartDeviceListFetchAction = {
+        type: 'START_DEVICE_LIST_FETCH';
+    };
+      
+    type Action = StartDeviceListFetchAction;
+
+    //reducer alustus
+    const [appState, dispatch] = useReducer(appReducer, initAppState);
+
+    function appReducer(state: AppState, action: Action ): AppState {
+        
+        switch( action.type ) {
+           case 'START_DEVICE_LIST_FETCH':
+            console.log("START_DEVICE_LIST_FETCH", action)
+            return {
+                ...state, 
+                deviceListFetchCommenced: true
+            } 
+        }
+
+
+        
+        
+
+        
+            
+
+        // return state;
+    }
 
     return(
         <div className='background'>
